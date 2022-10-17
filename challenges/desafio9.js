7689579678d8fe8c5a7db1487551b26cb8c5386e
ronaferr <ferreiraronaldocarlos@gmail.com>
2022-10-17 17:46:55 -0300

    query para produtos com menos de 500 calorias

---
+++ challenges/desafio9.js
@@ -0,0 +1,16 @@
+db.produtos.find(
+    {
+        valoresNutricionais: {
+            $elemMatch: {
+                tipo: "calorias",
+                quantidade: {
+                    $lt: 500, 
+                },
+            },
+        },
+        },
+    {
+        nome: 1,
+        _id: 0,
+    },
+);
