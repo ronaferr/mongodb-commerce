f01cbac502c1eaf98ffc7224976bbc429981b419
ronaferr <ferreiraronaldocarlos@gmail.com>
2022-10-17 18:46:37 -0300

    query para produtos que não sejam Big Mac e McChicken

---
+++ challenges/desafio11.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+    {
+        nome: {
+            $nin: ["Big Mac", "McChicken"],
+        },
+    },
+    {
+        nome: 1,
+        _id: 0,
+        vendidos: 1,
+        curtidas: 1,
+    },
+);
