5e41dd4a037984fc912c8fa88250557981af5a45
ronaferr <ferreiraronaldocarlos@gmail.com>
2022-10-17 19:34:20 -0300

    query para adicionar campo "CriadoPor"

---
+++ challenges/desafio13.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+    {},
+    {
+        $set: {
+            criadoPor: "Ronald McDonald",
+        },
+    },
+);
+db.produtos.find(
+    {},
+    {
+        nome: 1,
+        _id: 0,
+        criadoPor: 1,
+    },
+);
